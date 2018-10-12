51d078e35d9d74e71182e3eb5c30abf66a2bf429
mikhailf <mikhailf@mikhailf-HP-Laptop-15-bw0xx>
2018-10-12 04:01:58 +0300

    Черновой вариант. Вроде всё правильно

--- src/fileChecker/fileChecker.go
+++ src/fileChecker/fileChecker.go
@@ -6,24 +6,24 @@ import (
 	"io"
 	"time"
 )
-
+// Сущность, постоянно проверяющая рабочий каталог
 type FileChecker struct {
 	ticker *time.Ticker 
 }
-
-func appendWithFiles(oldFiles,newFiles []os.FileInfo) []os.FileInfo {
+// Функция добавляет один слайс в другой. Может, слишком велосипедно, но по-другому у меня не получилось
+func appendWithFiles(oldFiles,newFiles []string) []string {
 	for _, file := range newFiles {
 		oldFiles = append(oldFiles,file);
 	}
 	return oldFiles;
 }
-
-func getFiles(workDir* os.File) ([]os.FileInfo, error) {
-	var oldFiles,curFiles []os.FileInfo;
+// Функция для получения текущего списка файлов рабочего каталога  
+func getFiles(workDir* os.File) ([]string, error) {
+	var oldFiles,curFiles []string;
 	var err error;
-	oldFiles = make([]os.FileInfo,0,10);
+	oldFiles = make([]string,0,10);
 	
-	for curFiles, err = workDir.Readdir(10);err == nil;oldFiles = appendWithFiles(oldFiles,curFiles) {
+	for curFiles, err = workDir.Readdirnames(10);err == nil;oldFiles = appendWithFiles(oldFiles,curFiles) {
 		
 	}
 	if err != io.EOF {
@@ -31,36 +31,58 @@ func getFiles(workDir* os.File) ([]os.FileInfo, error) {
 	}
 	return oldFiles,nil
 }
-func checkThisDir(workDir *os.File) { 
-	
-}
 
-func errMsg(err error) {
-	log.Println("Some issue with work directory creating"+err.Error());	
+// Основная функция данного пакета. Получает текущий списко файлов, сравнивает его с предыдущим и выдает наружу
+// свежий или предыдущий, если они не изменились
+func checkThisDir(workDir *os.File,oldFiles []string) ([]string, error) {
+	var err error 
+	curFiles, err := getFiles(workDir)
+	if err != nil && err != io.EOF {
+		return nil,err
+	}
+	old_len := len(oldFiles)
+	cur_len := len(curFiles)
+	if old_len != cur_len {
+		return curFiles,nil
+	}
+	for i,d:= range oldFiles {
+		if d != curFiles[i] {
+			return curFiles, nil
+		}
+	}
+	return oldFiles,nil
 }
-
-func (fc *FileChecker) Start(fname string) error {
+// Вспомогательная фунекция - пишет лшибки в лог
+func errMsg(msg string, err error) {
+	log.Println(msg+err.Error());	
+}
+// Основная функция - регулярно проверяет рабочий каталог
+func (fc *FileChecker) Process(fname string) error {
 	var workDir *os.File
 	var err error
 	workDir, err = os.Open(fname);
 	if (err != nil) {
 		if !os.IsNotExist(err) {
-		 	errMsg(err);
+		 	errMsg("Some issue with work directory creating",err);
 		 	return err;
 		 }
 		 workDir, err = os.Create(fname);
 		 if (err != nil) {
-		 	errMsg(err);
+		 	errMsg("Some issue with work directory creating",err);
 		 	return err;
 		 }
 	}	
-//	oldFiles, err = getFiles(workDir)
+	curFiles, err := getFiles(workDir)
 	if err != nil {
 		return nil
 	}
 	fc.ticker = time.NewTicker(5*time.Second)
 	for _ = range fc.ticker.C {
-		checkThisDir(workDir)
+		oldFiles := curFiles;
+		curFiles, err = checkThisDir(workDir, oldFiles)
+		if err != nil {
+			errMsg("Error during file checking: ",err)
+		}
 	}
 	return nil;
 }
